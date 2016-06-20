2065f47fdb833d91f61cb104175f78b65d7962a2
David Anekstein <aneksteind@gmail.com>
2016-06-20 11:22:51 -0300

    added types

---
+++ Types.hs
@@ -0,0 +1,43 @@
+module Types where
+
+data Expr a = EVar Name
+            | ENum Int
+            | EConstr Int Int
+            | EAp (Expr a) (Expr a)
+            | ELet IsRec [(a, Expr a)] (Expr a)
+            | ECase (Expr a) [Alter a]
+            | ELam [a] (Expr a)
+            (deriving Show, Eq)
+
+type CoreExpr = Expr Name
+
+type Name = String
+
+type IsRec = Bool
+
+type Alter a = (Int, [a], Expr a)
+type CoreAlt = Alter Name
+
+type Program a = [ScDefn a]
+type CoreProgram = Program Name
+
+type ScDefn a = (Name, [a], Expr a)
+type CoreScDefn = ScDefn Name
+
+recursive :: IsRec
+recursive = True
+
+nonRecursive :: IsRec
+nonRecursive = False
+
+bindersOf :: [(a,b)] -> [a]
+bindersOf defns = [name | (name,rhs) <- defns]
+
+rhssOf :: [(a,b)] -> [b]
+rhssOf defns = [rhs | (name,rhs) <- defns]
+
+isAtomicExpr :: Expr a -> Bool
+isAtomicExpr (EVar v) = True
+isAtomicExpr (ENum n) = True
+isAtomicExpr e = False
+
