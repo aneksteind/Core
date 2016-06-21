0b5ec5037e2291741215e9221ae0f21dd801a276
David Anekstein <aneksteind@gmail.com>
2016-06-21 20:30:52 -0300

    pretty prints a parsed expression, updated test file

--- main.hs
+++ main.hs
@@ -1,14 +1,14 @@
 module Main where
 
 import Lexer
+import Parser
 import Pretty
 import Types
 import CorePrelude
 
 main :: IO ()
 main = do
-    --unParsed <- getContents
-    --let scanned = scanTokens unParsed
-    --print scanned
-    putStr $ pprint preludeDefs
-
+    unParsed <- getContents
+    let scanned = scanTokens unParsed
+    let parsed = parseTokens scanned
+    putStr $ pprint parsed
