86e952e4c0118b0301b666f46751196a4caa1f3f
David Anekstein <aneksteind@gmail.com>
2016-06-21 18:14:47 -0300

    updated for pretty printer

--- main.hs
+++ main.hs
@@ -1,10 +1,14 @@
 module Main where
 
-import Parser
 import Lexer
+import Pretty
+import Types
+import CorePrelude
 
-main :: IO
+main :: IO ()
 main = do
-    unParsed <- getContents
-    let parsed = parseTokens $ scanTokens unParsed :: [CoreExpr]
-    print parsed
+    --unParsed <- getContents
+    --let scanned = scanTokens unParsed
+    --print scanned
+    putStr $ pprint preludeDefs
+
