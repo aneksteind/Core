89647ae0bce94f6bcd87c0e9d1088feb3910b4f4
David Anekstein <aneksteind@gmail.com>
2016-06-23 11:21:36 -0300

    prints tokens as well as the pretty printed parsed program

--- main.hs
+++ main.hs
@@ -5,10 +5,13 @@ import Parser
 import Pretty
 import Types
 import CorePrelude
+import System.Environment
 
 main :: IO ()
 main = do
-    unParsed <- readFile "prelude.cor"
+    (file:_) <- getArgs
+    unParsed <- readFile file
     let scanned = scanTokens unParsed
+    print scanned
     let parsed = parseTokens scanned
     putStr $ pprint parsed
