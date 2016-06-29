55b643b3336b33b325356cc0bf71492e210e2b52
David Anekstein <aneksteind@gmail.com>
2016-06-29 14:25:32 -0300

    added prelude defs

--- Types.hs
+++ Types.hs
@@ -106,6 +106,18 @@ type GmCompiler = CoreExpr -> GmEnvironment -> GmCode
 
 type GmEnvironment = ASSOC Name Int
 
+--------------------------- PRELUDE ---------------------------
+
+preludeDefs :: CoreProgram
+preludeDefs = [ ("I", ["x"], EVar "x"),
+  ("K", ["x","y"], EVar "x"),
+  ("K1",["x","y"], EVar "y"),
+  ("S", ["f","g","x"], EAp (EAp (EVar "f") (EVar "x"))
+  (EAp (EVar "g") (EVar "x"))),
+  ("compose", ["f","g","x"], EAp (EVar "f")
+  (EAp (EVar "g") (EVar "x"))),
+  ("twice", ["f"], EAp (EAp (EVar "compose") (EVar "f")) (EVar "f")) ]
+
 
 
 
