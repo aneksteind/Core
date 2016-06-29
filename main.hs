9f426e27156f8f01ce032b297c6091864cab540d
David Anekstein <aneksteind@gmail.com>
2016-06-29 12:52:01 -0300

    now evaluates a core program, which must have a main method in order to work

--- main.hs
+++ main.hs
@@ -4,13 +4,14 @@ import Lexer
 import Parser
 import Pretty
 import Types
-import CorePrelude
 import System.Environment
 import GMachine
+import Compiler
 
 main :: IO ()
 main = do
     (file:_) <- getArgs
     unParsed <- readFile file
     let parsed = parseTokens $ scanTokens unParsed
-    putStr $ pprint parsed
+    print $ eval (compile parsed)
+--  putStr $ pprint parsed
